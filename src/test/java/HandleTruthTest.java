29fe2a6f65bef831418b1737350023b614e0539e
Haaris Nadeem <haaris2001@gmail.com>
2020-11-20 14:30:43 +0500

    working on first test

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -1,8 +1,16 @@
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HandleTruthTest {
     @Test
     public void makeshiftTest() {
+
+
         HandleTruth.wordCount();
+        System.out.println("Assert 'you' should be counted 18 times");
+        System.out.println("Is 'you' printed 18 times?" );
+
     }
 }
