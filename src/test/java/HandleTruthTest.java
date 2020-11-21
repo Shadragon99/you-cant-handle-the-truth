29977f9138f8957fd8bb09f065a1ccccc5b814fe
Haaris Nadeem <haaris2001@gmail.com>
2020-11-21 19:36:11 +0500

    Test 2 is complete

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -17,6 +17,13 @@ public class HandleTruthTest {
         System.out.print("Answer : " + ans.containsKey("you"));
         System.out.println();
 
+        //Test 2
+        System.out.println("Assert 'hello' should NOT be present in HashMap");
+        System.out.println("Is 'hello' present?");
+        System.out.print("Answer : " + ans.containsKey("hello"));
+        System.out.println();
+
+        //Test 3
         System.out.println("Assert 'you' should be present in HashMap");
         System.out.println("Is 'you' present?");
         System.out.print("Answer : " + ans.containsKey("you"));
