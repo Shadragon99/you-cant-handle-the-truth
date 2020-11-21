4717d3cd7698425b059ff8949dd7b96a8a3ddb53
Haaris Nadeem <haaris2001@gmail.com>
2020-11-21 19:43:54 +0500

    Test 3 is complete, it checks whether 'you' has been detected the right amount of times (18 times)

--- src/test/java/HandleTruthTest.java
+++ src/test/java/HandleTruthTest.java
@@ -4,12 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HandleTruthTest {
+
+    HashMap ans = HandleTruth.wordCount();
+
     @Test
     public void makeshiftTest() {
 
-        HashMap ans = HandleTruth.wordCount();
         //HandleTruth.wordCount();
-        //System.out.println("Here is answer: " + ans);
 
         //Test 1
         System.out.println("Assert 'you' should be present in HashMap");
@@ -28,7 +29,21 @@ public class HandleTruthTest {
         System.out.println("Is 'you' present?");
         System.out.print("Answer : " + ans.containsKey("you"));
 
+    }
+    @Test
+    public void makeshiftTest1() {
+        //Test 2
+        System.out.println("Assert 'hello' should NOT be present in HashMap");
+        System.out.println("Is 'hello' present?");
+        System.out.print("Answer : " + ans.containsKey("hello"));
+        System.out.println();
+    }
 
-
+    @Test
+    public void makeshiftTest2() {
+        //Test 3
+        //System.out.println("Assert 'you' should be present in HashMap 18 times");
+        //System.out.println("Is 'you' counted 18 times?");
+        System.out.print("Answer : (you , " + ans.get("you") + ") " + ans.containsKey("you"));
     }
 }
